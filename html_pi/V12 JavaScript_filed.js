84bb42edf8c8cd9e9402eadcfd29c6327682eea0
Edgardo Aguirre <aguirre5411@gmail.com>
2021-07-18 22:45:36 -0300

    Se elimina carpeta HTML PI

--- html_pi/V12 JavaScript_filed.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-function saludo() {
-	alert("Otra vez pongo aqui que hay de nuevo?");
-}
-function movimientoderaton() {
-	alert("Como te gusta mover el raton a vos heeeee");
-}
-
