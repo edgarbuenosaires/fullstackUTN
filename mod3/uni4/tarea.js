94fd570ecae4d6c3648d6591ca2deb723b343a30
Edgardo Aguirre <aguirre5411@gmail.com>
2021-10-03 01:30:11 -0300

    ejercicio 1 ok

---
+++ mod3/uni4/tarea.js
@@ -0,0 +1,19 @@
+//Ejercicio 1
+var fondo = document.getElementById('cuadroAzul');
+fondo.addEventListener('click', function () {
+    document.body.style.backgroundColor = '#0066CC';
+})
+var fondo = document.getElementById('cuadroVerde');
+fondo.addEventListener('click', function () {
+    document.body.style.backgroundColor = '#009846';
+})
+var fondo = document.getElementById('cuadroRojo');
+fondo.addEventListener('click', function () {
+    document.body.style.backgroundColor = '#f75e25';
+})
+
+//Ejercicio 2
+
+
+
+//Ejercicio 3
