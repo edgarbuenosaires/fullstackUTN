5e4df00ef555c11f1d6c6d080492e0264d96d6cf
Edgardo Aguirre <aguirre5411@gmail.com>
2021-10-05 19:45:48 -0300

    Se completa tarea Unid 4 del Mod 3

--- mod3/uni4/tarea.js
+++ mod3/uni4/tarea.js
@@ -13,7 +13,48 @@ fondo.addEventListener('click', function () {
 })
 
 //Ejercicio 2
+const mensaje = document.getElementById('mensaje');
+const contador = document.getElementById('contador');
 
-
+mensaje.addEventListener('input', function (e) {
+    const target = e.target;
+    const longitudMax = target.getAttribute('maxlength');
+    const longitudAct = target.value.length;
+    contador.innerHTML = `${longitudAct}/${longitudMax}`;
+});
 
 //Ejercicio 3
+
+var alumnos = [
+    {
+        nombre: 'Juan Gomez',
+        nota: 7
+    }, {
+        nombre: 'Pedro Rodriguez',
+        nota: 4
+    }, {
+        nombre: 'Roxana García',
+        nota: 8
+    }, {
+        nombre: 'Luciano Lopez',
+        nota: 5
+    }, {
+        nombre: 'Fernanda Gimenez',
+        nota: 6
+
+    }, {
+        nombre: 'Florencia Martinez',
+        nota: 10
+    }, {
+        nombre: 'Raul Sanchez',
+        nota: 7
+    }, {
+        nombre: 'Sandra Figueroa',
+        nota: 8
+    }
+];
+
+var aprobados = alumnos.filter(function (alumno) {
+    return alumno.nota >= 7;
+});
+console.log(aprobados);
