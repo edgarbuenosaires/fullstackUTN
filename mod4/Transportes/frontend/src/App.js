44d1bb78f010eb5f676a2e7e4b8bd9f3164ed726
Edgardo Aguirre <aguirre5411@gmail.com>
2021-10-12 00:10:10 -0300

    se corrige contactos

--- mod4/Transportes/frontend/src/App.js
+++ mod4/Transportes/frontend/src/App.js
@@ -9,7 +9,7 @@ import Header from './components/layout/Header';
 import Nav from './components/layout/Nav';
 import Footer from './components/layout/Footer';
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 
 function App() {
   return (
