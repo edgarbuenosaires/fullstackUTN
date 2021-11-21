07f5c6276f07fb8e771ebbe9b2fdfdd1cb4814d6
Edgardo Aguirre <aguirre5411@gmail.com>
2021-11-21 16:10:55 -0300

    conexion front y back funcionando

--- mod4/Transportes/frontend/src/pages/NovedadesPage.js
+++ mod4/Transportes/frontend/src/pages/NovedadesPage.js
@@ -1,14 +1,36 @@
 import React from "react";
 
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import NovedadItem from "../components/novedades/NovedadItem";
+
 import '../styles/components/pages/NovedadesPage.css';
 
 const NovedadesPage = (props) => {
+
+    const [loading, setLoading] = useState(false);
+    const [novedades, setNovedades] = useState([]);
+
+    useEffect(() => {
+        const cargarNovedades = async () => {
+            setLoading(true);
+
+            const response = await axios.get('http://localhost:3000/api/novedades');
+            setNovedades(response.data);
+            setLoading(false);
+        };
+        cargarNovedades();
+    }, []);
+
     return (
         <section className="holder">
             <h2>Novedades</h2>
-            <h3>titulo</h3>
-            <h4>subtitulo</h4>
-            <p>cuerpo</p>
+            {loading ? (
+                <p>Cargando...</p>
+            ) : (
+                novedades.map(item => <NovedadItem key={item.id} title={item.titulo} subtitle={item.subtitulo}
+                    imagen={item.imagen} body={item.cuerpo} />)
+            )}
         </section>
     );
 }
