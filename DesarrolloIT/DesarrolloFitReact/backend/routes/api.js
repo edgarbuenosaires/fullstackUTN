7fd778cd8c6ac6089cd2fb3b550cc1f9a83925aa
Edgardo Aguirre <aguirre5411@gmail.com>
2021-11-30 15:20:52 -0300

    front and back funcionando OK

--- DesarrolloIT/DesarrolloFitReact/backend/routes/api.js
+++ DesarrolloIT/DesarrolloFitReact/backend/routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mealsModel = require('./../models/mealsModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
 
 router.get('/meals', async function (req, res, next) {
     let meals = await mealsModel.getMeals();
@@ -26,4 +27,31 @@ router.get('/meals', async function (req, res, next) {
     });
     res.json(meals);
 })
+
+// Contacto
+
+router.post('/contacto', async (req, res) => {
+    const mail = {
+        to: 'aguirre5411@gmail.com',
+        subject: 'Contacto web',
+        html: `${req.body.nombre} se contacto a traves de la web y quiere mas informacion a este correo: ${req.body.email}<br> Ademas, hizo el siguiente comentario: ${req.body.mensaje}<br> Su telefono es: ${req.body.telefono}`
+    }
+
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth:{
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        message: 'Mensaje enviado'
+    });
+});
+
 module.exports = router;
--- DesarrolloIT/DesarrolloFitReact/frontend/src/pages/ContactoPage.js
+++ DesarrolloIT/DesarrolloFitReact/frontend/src/pages/ContactoPage.js
@@ -1,34 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 import '../styles/components/pages/ContactoPage.css';
 
 const ContactoPage = (props) => {
+
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        mensaje: ''
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true)
+        const response = await axios.post('http://localhost:3000/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if (response.data.error === false) {
+            setFormData(initialForm)
+        }
+    }
+
     return (
         <main className="holder">
             <div className="columna left">
                 <h2>Comunicate con nosotros</h2>
-                <form action="" method="" class="formulario">
+                <form action="/contacto" method="post" className="formulario" onSubmit={handleSubmit}>
                     <p>
-                        <label for="">Nombre</label>
-                        <input type="text" />
+                        <label>Nombre</label>
+                        <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} />
                     </p>
                     <p>
-                        <label for="">Email</label>
-                        <input type="email" />
+                        <label>Email</label>
+                        <input type="text" name="email" value={formData.email} onChange={handleChange} />
                     </p>
                     <p>
-                        <label for="">Telefono</label>
-                        <input type="text" />
+                        <label>Telefono</label>
+                        <input type="text" name="telefono" value={formData.telefono} onChange={handleChange} />
                     </p>
                     <p>
-                        <label for="comentario">Comentario</label>
-                        <textarea id="comentario"></textarea>
+                        <label>Comentario</label>
+                        <textarea name="mensaje" value={formData.mensaje} onChange={handleChange}></textarea>
                     </p>
 
-                    {/* OJO ACA ESTE CLASSNAME */}
-                    <p className="acciones"><input type="submit" value="Enviar" /></p>
+                    {sending ? <p>Enviando...</p> : null}
+                    {msg ? <p>{msg}</p> : null}
+
+                    <p className="centrar"><input type="submit" value="Enviar" /></p>
                 </form>
             </div>
-            <div class="columna right">
+            <div className="columna right">
                 <h2>Otras vias de contacto</h2>
                 <p>Tambien se pueden comunicar con nosotros ...</p>
                 <ul>
